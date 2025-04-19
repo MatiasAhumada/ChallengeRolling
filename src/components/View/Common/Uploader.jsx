7955e9342518b5afb720cb47d886b60089e43541
Matiasahumada <pagosptcmatias@gmail.com>
2025-04-18 23:32:23 -0300

    downgradee a react 18 para mayor compatibilidad, agregue validaciones y agregue el modal para coemnzar con la imagen

--- src/components/View/Common/Uploader.jsx
+++ src/components/View/Common/Uploader.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { InboxOutlined, DownloadOutlined } from "@ant-design/icons";
-import { Button, Input, Upload } from "antd";
+import { InboxOutlined, EyeFilled } from "@ant-design/icons";
+import { Button, Input, Upload, Modal } from "antd";
 import { Row } from "react-bootstrap";
 import Swal from "sweetalert2";
 const { Dragger } = Upload;
@@ -8,6 +8,10 @@ const { Dragger } = Upload;
 const Uploader = () => {
   const [fileList, setFileList] = useState([]);
   const [formVisible, setFormvisible] = useState(false);
+  const [nombre, setNombre] = useState("");
+  const [dia, setDia] = useState("");
+  const [mes, setMes] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const props = {
     name: "file",
     multiple: false,
@@ -46,20 +50,41 @@ const Uploader = () => {
         //message.success(`${info.file.name} subido correctamente.`);
       }
       if (status === "error") {
-        Swal.fire({
-          icon: "error",
-          title: "Error!",
-          text: `${info.file.name} fallo al subir.`,
-          showConfirmButton: false,
-          timer: 1000,
-        });
+        // Swal.fire({
+        //   icon: "error",
+        //   title: "Error!",
+        //   text: `${info.file.name} fallo al subir.`,
+        //   showConfirmButton: false,
+        //   timer: 1000,
+        // });
+        setFormvisible(true);
       }
+      if (status === "removed") {
+        setFormvisible(false);
+      }
+      console.log(info);
     },
     onDrop(e) {
       console.log("Archivo arrastrado:", e.dataTransfer.files);
     },
   };
+  const isFormValid = () => {
+    const diaNum = parseInt(dia);
+    return nombre.trim() !== "" && nombre.length <= 80 && !isNaN(diaNum) && diaNum >= 0 && diaNum <= 31 && mes.trim() !== "" && mes.length <= 15;
+  };
+ 
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsModalOpen(true);
+  };
 
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="w-84 pt-30">
@@ -73,16 +98,24 @@ const Uploader = () => {
           </div>
         </Dragger>
       </div>
-      <div className="w-84 flex mt-5">
-        <Input placeholder="Nombre" />
-        <Input placeholder="Dia" className="w-auto" />
-        <Input placeholder="Mes" />
-      </div>
-      <div className="mt-5 ">
-        <Button type="primary" icon={<DownloadOutlined />} className="rounded-3xl">
-          Download
-        </Button>
-      </div>
+      {formVisible && (
+        <>
+          <form className="w-84 flex mt-5" id="form" >
+            <Input placeholder="Nombre" name="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+            <Input placeholder="Dia" className="w-auto" name="dia" value={dia} onChange={(e) => setDia(e.target.value)} />
+            <Input placeholder="Mes" name="mes" value={mes} onChange={(e) => setMes(e.target.value)} />
+          </form>
+          <Button onClick={handleClick} disabled={!isFormValid()} type="primary" shape="round" className="p-3 mt-3" icon={<EyeFilled />}>
+            Previsualizar
+          </Button>
+          
+          <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </Modal>
+        </>
+      )}
     </div>
   );
 };
